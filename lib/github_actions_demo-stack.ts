acf5a6bdbbe3b24566c8d6d6ab4e788e4de582a8
Sean Montague <stmontague1@gmail.com>
2024-03-03 10:03:47 -0600

    Update sample bucket to unique name

--- lib/github_actions_demo-stack.ts
+++ lib/github_actions_demo-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GithubActionsDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubActionsDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    super(scope, id, props)
+    new s3.Bucket(this, 'SampleBucket141242141241241', {
+      versioned: false,
+    });
   }
 }

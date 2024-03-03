94c59a5e9d13e167118fd576f3d13fedb27766a1
Sean Montague <stmontague1@gmail.com>
2024-03-03 10:44:36 -0600

    Add SQS queue and set-up the delivery of notifications when objects are created

--- lib/github_actions_demo-stack.ts
+++ lib/github_actions_demo-stack.ts
@@ -1,12 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3Notifications from 'aws-cdk-lib/aws-s3-notifications'
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 
 export class GithubActionsDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
-    new s3.Bucket(this, 'SampleBucket141242141241241', {
+    const bucket = new s3.Bucket(this, 'SampleBucket', {
       versioned: false,
     });
+
+    const queue = new sqs.Queue(this, 'SampleQueue');
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3Notifications.SqsDestination(queue));
   }
 }
